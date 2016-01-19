1b7e44a7ee3783f02452a7187f699196d0bdbcfe
Justin Basinger <justin.m.basinger@gmail.com>
2016-01-18 20:23:25 -0500

    Adding ACE dependencies

--- browser_files/main.js
+++ browser_files/main.js
@@ -1,5 +1,9 @@
-var app = angular.module('editor',['ui.bootstrap']);
+var app = angular.module('editor',['ui.bootstrap','ui.ace']);
 
 app.controller('main',function($scope){
+
   $scope.helloElectron = "Hello Electron!";
+
+
+
 });
