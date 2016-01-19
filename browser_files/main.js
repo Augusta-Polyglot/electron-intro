0fe9361cce63b5348463d97f7189df6171f94a30
Justin Basinger <justin.m.basinger@gmail.com>
2016-01-18 21:33:26 -0500

    Basic editor complete

--- browser_files/main.js
+++ browser_files/main.js
@@ -1,7 +1,52 @@
+
+var ipc = require('electron').ipcRenderer;
+
 var app = angular.module('editor',['ui.bootstrap','ui.ace']);
+
 
 app.controller('main',function($scope){
 
-  $scope.helloElectron = "Hello Electron!";
+  $scope.tabs = [];
+
+  $scope.openFile = function(){
+    ipc.send('openFile');
+  }
+
+  $scope.newFile = function(){
+    $scope.tabs.push({
+      filename:"untitled",
+      data: "",
+      active: true
+    });
+  }
+
+  $scope.saveFile = function(){
+    $scope.tabs.forEach(function(tab, index){
+
+      if(!tab.active){
+        return;
+      }
+
+      ipc.send('saveFile', $scope.tabs[index].data, index);
+
+    });
+  }
+
+  ipc.on('openedFile', function(evt, fileData){
+    $scope.$apply(function(){
+      $scope.tabs.push({
+        filename: fileData.filename,
+        data: fileData.data,
+        active: true
+      });
+    });
+  });
+
+  ipc.on('savedFile', function(evt, filename, index){
+    $scope.$apply(function(){
+      var tab = $scope.tabs[index];
+      tab.filename = filename;
+    });
+  });
 
 });
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+
+//Node requires
+var fs = require('fs');
+
+//Electron requires
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var ipc = require('electron').ipcMain;
+var dialog = require('dialog');
 
 var editorWindow;
 
@@ -15,5 +22,45 @@ app.on('ready', function(){
 
   editorWindow.loadURL('file://' + __dirname + '/main.html');
   editorWindow.show();
+
+  ipc.on('openFile', function(evt){
+    dialog.showOpenDialog({
+      title: 'Open a file'
+    }, function(filenames){
+
+      if(!filenames){
+        return;
+      }
+
+      filenames.forEach(function(filename){
+        fs.readFile(filename, function(err, data){
+          if(err){
+            console.log(err.toString());
+          } else {
+            evt.sender.send('openedFile', {
+              filename: filename,
+              data: data.toString()
+            });
+          }
+        });
+      });
+
+
+    });
+  });
+
+  ipc.on('saveFile', function(evt, fileData, index){
+    dialog.showSaveDialog({
+      title: 'Save File'
+    }, function(filename){
+      fs.writeFile(filename, fileData, function(err){
+        if(err){
+          console.log(err);
+          return;
+        }
+        evt.sender.send('savedFile', filename, index);
+      })
+    });
+  });
 
 });
